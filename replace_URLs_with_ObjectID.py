4093531f2634c8b94a012d50e5f75374ef24a837
Seb Manley <sebastian.amanley@gmail.com>
2023-12-14 14:59:26 +0000

    Latest

--- replace_URLs_with_ObjectID.py
+++ replace_URLs_with_ObjectID.py
@@ -13,12 +13,19 @@ class replace_pilot_urls(Starship_Pilots):
         super().__init__()
 
     def replace_urls(self):
+        pilot_names = []
+        for pilot in self.list:
+            for pilot_url in pilot:
+                if len(pilot[pilot_url]) >= 1:
+                    pilot_details = requests.get(pilot[pilot_url])
+                    json_pilot_details = pilot_details.json()
+                    pilot_names.append({json_pilot_details['starships']: json_pilot_details['name']})
 
-            pilot_objectids = []
-            for pilot in pilot_names:
-                character = db.characters.find_one({"name": pilot}, {"_id": 1})
-                if character:
-                    pilot_objectids.append(character["_id"])
+        pilot_objectids = []
+        for pilot in pilot_names:
+            character = db.characters.find_one({"name": pilot}, {"_id": 1})
+            if character:
+                pilot_objectids.append(character["_id"])
 
 
 
