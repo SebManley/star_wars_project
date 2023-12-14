f3be93bd7cd59fd28fc8c40ad8d894ce2a2c0ac8
Jesse <jr2g17@soton.ac.uk>
2023-12-14 14:16:02 +0000

    setup

--- replace_URLs_with_ObjectID.py
+++ replace_URLs_with_ObjectID.py
@@ -1,2 +1,15 @@
 import requests
+import pymongo
+import starship_pilot
+
+
+client = pymongo.MongoClient()
+
+db = client['starwars']
+
+class replace_pilot_urls(Starship_Pilots):
+
+    def __init__(self):
+
+
 
