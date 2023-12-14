86a94311fd02248e562a6539a63e0dbe5272cccb
Richard Van Parys <richard.vanparys@btinternet.com>
2023-12-14 15:24:19 +0000

    updated tests.py for both .py files

--- tests.py
+++ tests.py
@@ -1,9 +1,10 @@
-# from starship_pilots import *
+from starship_pilots import *
 from swapi_call import *
 import unittest
+from unittest.mock import patch
 
 
-class TestYourClassInitialization(unittest.TestCase):
+class TestSWAPICall(unittest.TestCase):
 
     def test_default_swapi_base_url(self):
         instance = StarWarsAPI()
@@ -16,29 +17,45 @@ class TestYourClassInitialization(unittest.TestCase):
         self.assertEqual(instance.swapi_base_url, custom_url,
                          "Custom swapi_base_url is not set correctly")
 
+    @patch('swapi_call.r.get')
+    def test_get_starships_data(self, mock_get):
+        # Mocking the response of the requests.get method
+        mock_response = unittest.mock.Mock()
+        mock_response.json.return_value = {'results': [{'name': 'Starship1'}, {'name': 'Starship2'}]}
+        mock_get.return_value = mock_response
+
+        # Creating an instance of Starship_Pilots
+        star_wars_api = StarWarsAPI()
+
+        # Calling the method we want to test
+        result = star_wars_api.get_starships_data()
+
+        # Assertions
+        mock_get.assert_called_once_with('https://swapi.dev/api/starships/')
+        self.assertEqual(result, {'results': [{'name': 'Starship1'}, {'name': 'Starship2'}]})
+
+
+class TestStarshipPilotsInitialization(unittest.TestCase):
+
+    @patch('starship_pilots.Starship_Pilots.get_starships_data')
+    def test_starship_pilots_list(self, mock_get_starships_data):
+        # Mocking the response of the get_starships_data method
+        mock_get_starships_data.return_value = {'results': [
+            {'name': 'Starship1', 'pilots': ['Pilot1', 'Pilot2']},
+            {'name': 'Starship2', 'pilots': ['Pilot3', 'Pilot4']},
+        ]}
+
+        # Creating an instance of Starship_Pilots
+        starship_pilots = Starship_Pilots()
+
+        expected_list = [
+            {'Starship1': ['Pilot1', 'Pilot2']},
+            {'Starship2': ['Pilot3', 'Pilot4']},
+        ]
+
+        self.assertEqual(starship_pilots.list, expected_list,
+                         "Starship_Pilots list is not initialized correctly")
+
 
 if __name__ == '__main__':
     unittest.main()
-
-# class TestStarshipPilotsInitialization(unittest.TestCase):
-#
-#     def test_starship_pilots_list(self):
-#         # Assuming get_starships_data is a function that returns a sample starships data
-#         def mock_get_starships_data():
-#             return {'results': [
-#                 {'name': 'Starship1', 'pilots': ['Pilot1', 'Pilot2']},
-#                 {'name': 'Starship2', 'pilots': ['Pilot3', 'Pilot4']},
-#             ]}
-#
-#         # Monkey-patching the get_starships_data function to return the mock data
-#         with unittest.mock.patch('starship_pilots.get_starships_data', side_effect=mock_get_starships_data):
-#             starship_pilots = Starship_Pilots()
-#
-#         expected_list = [
-#             {'Starship1': ['Pilot1', 'Pilot2']},
-#             {'Starship2': ['Pilot3', 'Pilot4']},
-#             # Add more expected data as needed
-#         ]
-#
-#         self.assertEqual(starship_pilots.list, expected_list,
-#                          "Starship_Pilots list is not initialized correctly")
