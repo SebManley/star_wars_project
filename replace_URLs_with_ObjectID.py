346d27b8b9ab249fb4f35e43a4b7c21a89a9edcb
Seb Manley <sebastian.amanley@gmail.com>
2023-12-14 15:33:36 +0000

    Testing pilot IDs

--- replace_URLs_with_ObjectID.py
+++ replace_URLs_with_ObjectID.py
@@ -12,20 +12,27 @@ class replace_pilot_urls(Starship_Pilots):
     def __init__(self):
         super().__init__()
 
-    def replace_urls(self):
+    def pilot_ids(self):
         pilot_names = []
-        for pilot in self.list:
-            for pilot_url in pilot:
-                if len(pilot[pilot_url]) >= 1:
-                    pilot_details = requests.get(pilot[pilot_url])
+        for starships in self.list:
+            pilots = starships.values()
+            if len(starships[pilots]) >= 1:
+                driver_dict = {starships: []}
+                for pilot in pilots:
+                    pilot_details = requests.get(pilot)
                     json_pilot_details = pilot_details.json()
-                    pilot_names.append({json_pilot_details['starships']: json_pilot_details['name']})
+                    driver_dict[starships].append(json_pilot_details['name'])
+                pilot_names.append(driver_dict)
 
         pilot_objectids = []
-        for pilot in pilot_names:
-            character = db.characters.find_one({"name": pilot}, {"_id": 1})
-            if character:
-                pilot_objectids.append(character["_id"])
+        for starship in pilot_names:
+            pilot_ids_dict = {starship: []}
+            name_list = starship.values()
+            for name in name_list:
+                character = db.characters.find_one({"name": name}, {"_id": 1})
+                if character:
+                    pilot_ids_dict[starship].append(character["_id"])
+            pilot_objectids.append(pilot_ids_dict)
 
 
 
