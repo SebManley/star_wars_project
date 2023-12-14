999973fad97b96ad6a689ad5de0eb2acfd9e9d04
Jesse <jr2g17@soton.ac.uk>
2023-12-14 14:26:35 +0000

    Pseudocode

--- replace_URLs_with_ObjectID.py
+++ replace_URLs_with_ObjectID.py
@@ -10,6 +10,17 @@ db = client['starwars']
 class replace_pilot_urls(Starship_Pilots):
 
     def __init__(self):
+        super().__init__()
 
+    def replace_urls(self):
+        for starship in self.list:
+            pilot_urls = starship.get("pilots", [])
+            pilot_objectids = []
+            for pilot_url in pilot_urls:
+                character = db.characters.find_one({"url": pilot_url})
+                if character:
+                    pilot_objectids.append(character["_id"])
+            starship["pilots"] = pilot_objectids
+        return self.list
 
 
